9bf1580bb8ed6bb2e816eb5159487b9982a5d3af
Vitalii Fedorenko <109212930+raogpip@users.noreply.github.com>
2023-07-05 13:50:23 +0200

    Delete ConsoleApp1 directory

--- ConsoleApp1/Program.cs
+++
@@ -1,8 +0,0 @@
-﻿using Diploma.Domain.Models;
-using Diploma.Domain.Services;
-using Diploma.EntityFramework;
-using Diploma.EntityFramework.Services;
-
-IDataService<Student> userService = new GenericDataService<Student>(new SchoolDbContextFactory());
-
-Console.WriteLine(userService.Delete(1).Result);
